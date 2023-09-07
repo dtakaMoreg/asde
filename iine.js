618e00e47f9f4b69599d8194de7b7258246c7cbd
dtakaMoreg <ch2chanchan@gmail.com>
2023-09-07 13:31:57 +0900

    Add files via upload

--- iine.js
+++ iine.js
@@ -34,7 +34,7 @@ javascript:(function(){
             alert("max");
         }else{
             var newtab =[];
-            for(var i=0;i<5;i++){
+            for(var i=0;i<1;i++){
                 if(links.length > window.counter){
                     newtab[i] = window.open(links[window.counter], '_blank');
                     window.counter++;
