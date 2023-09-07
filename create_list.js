505d97076be17051bc0bc74ff7cca7ea04624a84
dtakaMoreg <ch2chanchan@gmail.com>
2023-09-07 11:02:33 +0900

    Add files via upload

--- create_list.js
+++ create_list.js
@@ -1,29 +1,37 @@
 javascript:(function() {
-  var links = document.getElementsByTagName('a');
-  var linkTexts = [];
-  var cnt=0;
-  for (var i = 0; i < links.length; i++) {
-    hreft=links[i].getAttribute("href");
-    if (hreft != null && hreft.includes("/items")) {
-        cnt++;
-	    linkTexts.push('<a href="https://room.rakuten.co.jp' + hreft + '">User_' + cnt + "</a></br>" );
-	}
-  }
+    var links = document.getElementsByTagName('a');
+    var linkTexts = [];
+    var tmplst = [];
+    var cnt=0;
 
-  var textData = linkTexts.join('\n');
-  var blob = new Blob([textData], { type: 'text/html' });
-  var url = URL.createObjectURL(blob);
+    for (var i = 0; i < links.length; i++) {
+        hreft=links[i].getAttribute("href");
+        if (hreft != null && hreft.length > 6 && hreft.includes("/items")) {
+            tmplst.push('https://room.rakuten.co.jp' + hreft);
+        }
+    }
 
-  var a = document.createElement('a');
-  a.href = url;
-  a.download = 'link_text.html';
-  a.style.display = 'none';
-  document.body.appendChild(a);
+    tmplst = Array.from(new Set(tmplst));
 
-  a.click();
+    linkTexts.push('<html lang="ja">');
+    for (var i = 0; i < tmplst.length; i++) {
+        linkTexts.push('<a href="' + tmplst[i] + '">User_' + (i+1) + '</a></br>');
+    }
+    linkTexts.push('</html>');
+    var textData = linkTexts.join('\n');
+    var blob = new Blob([textData], { type: 'text/html' });
+    var url = URL.createObjectURL(blob);
 
-  window.addEventListener('focus', function() {
-    URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  });
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'link_text.html';
+    a.style.display = 'none';
+    document.body.appendChild(a);
+
+    a.click();
+
+    window.addEventListener('focus', function() {
+        URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+    });
 })();
