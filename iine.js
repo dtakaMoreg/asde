ed0dd3a51dfb32b3adb9bb21a975f9a3184b0522
dtakaMoreg <ch2chanchan@gmail.com>
2023-09-07 11:24:22 +0900

    Add files via upload

---
+++ iine.js
@@ -0,0 +1,40 @@
+javascript:(function(){
+    
+    //変数を初期化
+    if(typeof window.counter === 'undefined'){
+        window.counter = 0;
+    }
+    
+    if(typeof window.success_count === 'undefined'){
+        window.success_count = 0;
+    }
+    
+    if(typeof window.max_count === 'undefined'){
+        window.max_count = 0;
+    }
+
+    //roomを開いているなら
+    if (window.location.href.indexOf('https://room.rakuten.co.jp/') !== -1) {
+        //クリック処理
+        var buttonElements = document.querySelectorAll("button");
+        var like = buttonElements[4].querySelector("div");
+        if(like.className.includes("color-white")){
+            like.click();
+            window.success_count++;
+        }
+        //とじる
+        window.close();
+
+    } else {
+
+        var links = document.querySelectorAll('a');
+        window.max_count = links.length
+            
+        if(links.length <= window.counter){
+            alert("max");
+        }else{
+            window.open(links[window.counter], '_blank');
+            window.counter++;
+        }
+    }
+})();
