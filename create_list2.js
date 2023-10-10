0f236d9cbd9b49107a1914394871f2c796e9c3f8
dtakaMoreg <ch2chanchan@gmail.com>
2023-10-10 20:22:58 +0900

    Update create_list2.js

--- create_list2.js
+++ create_list2.js
@@ -50,7 +50,7 @@ javascript:(function() {
         tmplst = Array.from(new Set(tmplst));
         const now = new Date();
         const formattedDate = now.toISOString().replace(/[-T:.Z]/g, "").slice(0, 14); // yyyymmddhhmmss形式にフォーマット
-        fileName = `list_${formattedDate}.txt`; // ファイル名は適宜変更してください
+        fileName = `list_${formattedDate}_`; // ファイル名は適宜変更してください
 
         
         // ダウンロードを開始します
