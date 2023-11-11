bb541916d57b400a95c17d896c4d6b69e17a5d43
dtakaMoreg <ch2chanchan@gmail.com>
2023-11-11 09:24:19 +0900

    Update kai_iine_9cory.js

--- kai_iine_9cory.js
+++ kai_iine_9cory.js
@@ -1,1 +1,14 @@
-a
+javascript:(function(){
+
+    //ローカルストレージを表示
+    var iineClick = localStorage.getItem("iineClick");
+
+    var d = new Date(parseInt(nextResetTime));
+
+    // 日本標準時 (GMT+9) に設定
+
+    d.setHours(d.getHours() + 9);
+    var dstr = d.toISOString().slice(0, 19).replace("T", " ");
+    alert("nextResetTime[" + dstr + "] iineClick[" + iineClick + "] collectedURLs[" + collectedURLs.length + "] nextURLNo[" + nextURLNo + "]")
+
+})();
