e40c192d53faf4471e3dafcd73dd448c0aebe003
dtakaMoreg <ch2chanchan@gmail.com>
2023-11-17 12:04:38 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,19 @@
 javascript:(function() {
   const allText = document.body.innerText;
-  const hashtagMatches = allText.match(/#\w+/g);
+
+  // 日本語のハッシュタグを含む正規表現
+  const hashtagMatches = allText.match(/#[^\s#]+/g);
+
+  if (!hashtagMatches) {
+    alert('ハッシュタグが見つかりませんでした。');
+    return;
+  }
+
+  // 重複を排除してユニークなハッシュタグを収集
   const uniqueHashtags = [...new Set(hashtagMatches)];
+
+  // Local Storageに格納
   localStorage.setItem('collectedHashtags', JSON.stringify(uniqueHashtags));
+
   alert('ハッシュタグを収集し、Local Storageに格納しました。');
 })();
