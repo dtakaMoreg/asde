4d2489cd8ee98c5dfb079eb406a0dfc23306494d
dtakaMoreg <ch2chanchan@gmail.com>
2023-12-02 22:37:43 +0900

    Update room_click.js

--- room_click.js
+++ room_click.js
@@ -1,6 +1,10 @@
 javascript:(function() {
-  var elements = document.getElementsByClassName('susumeruRoomIcon');
-  if (elements.length > 0) {
-    elements[0].click();
-  }
+ // クラス名に "share-link" を含む要素を取得
+ var elementsWithShareLinkClass = document.querySelectorAll('[class*="share-link"]');
+
+// 要素が存在するか確認
+if (elementsWithShareLinkClass.length > 0) {
+  // 最初の要素をクリック（適宜変更可能）
+  elementsWithShareLinkClass[0].click();
+} 
 })();
