788ce54ce6e69fd0065426375d9ef9d896cc9912
dtakaMoreg <ch2chanchan@gmail.com>
2023-12-02 22:56:56 +0900

    Update room_click.js

--- room_click.js
+++ room_click.js
@@ -1,10 +1,24 @@
-javascript:(function() {
- // クラス名に "share-link" を含む要素を取得
- var elementsWithShareLinkClass = document.querySelectorAll('[class*="share-link"]');
+// クラス名が "bookmark-share-icon" の<a>タグを取得
+var bookmarkShareIconLink = document.querySelector('a[class*="bookmark-share-icon"]');
 
 // 要素が存在するか確認
-if (elementsWithShareLinkClass.length > 0) {
-  // 最初の要素をクリック（適宜変更可能）
-  elementsWithShareLinkClass[0].click();
-} 
-})();
+if (bookmarkShareIconLink) {
+  // <a>タグをクリック
+  bookmarkShareIconLink.click();
+
+  // 2秒待機
+  setTimeout(function() {
+    // クラス名が "susumeruRoomIcon" の要素を取得
+    var susumeruRoomIcon = document.querySelector('.susumeruRoomIcon');
+
+    // 要素が存在するか確認
+    if (susumeruRoomIcon) {
+      // 要素をクリック
+      susumeruRoomIcon.click();
+    } else {
+      console.log('susumeruRoomIconの要素が見つかりません');
+    }
+  }, 2000); // 2秒待機
+} else {
+  console.log('bookmark-share-iconの<a>タグが見つかりません');
+}
