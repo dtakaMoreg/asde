bc36770ae107440850bc49eb004f904b305d99ed
dai <dai@dai>
2023-12-04 12:38:21 +0900

    new room hidden

---
+++ room_hidden.js
@@ -0,0 +1,6 @@
+javascript: (function() {
+  var elements = document.getElementsByClassName('collect-notification');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = 'none';
+  }
+})();
