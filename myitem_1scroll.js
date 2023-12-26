e7ac665797acf3794f5ef6574edee200c02cd70f
dai <dai@dai>
2023-12-26 21:17:56 +0900

    wait 3s

--- myitem_1scroll.js
+++ myitem_1scroll.js
@@ -1,42 +1,31 @@
 javascript:(function() {
+    setTimeout(function() {
+        let favoriteIndexString = localStorage.getItem('favoriteIndex');
+        let favoriteIndex = favoriteIndexString ? parseInt(favoriteIndexString, 10) : 0;
+        let containerElements = document.querySelectorAll('img[class*="image"]');
+        let index = Math.floor(favoriteIndex / 2);
+        let offset = 0;
 
-    // localStorageからfavoriteIndexを取得
-    let favoriteIndexString = localStorage.getItem('favoriteIndex');
+        if (favoriteIndex % 2 === 0) {
+            offset = 2;
+        } else {
+            offset = Math.floor((containerElements.length - 2) / 2) + 2;
+        }
 
-    // favoriteIndexが存在しない、または値がnull/undefinedの場合、0を設定
-    let favoriteIndex = favoriteIndexString ? parseInt(favoriteIndexString, 10) : 0;
+        let num = offset + index;
 
-    // "container" を含むクラス名を持つ全ての div 要素を取得
-    let containerElements = document.querySelectorAll('img[class*="image"]');
+        if (containerElements.length >= (num + 1)) {
+            let fourteenthContainerElement = containerElements[num];
 
-    // 位置を特定
-    // indexは0から
-    // 
-    let index = Math.floor(favoriteIndex / 2);
-    let offset = 0;
-    
-    if (favoriteIndex % 2 === 0){
-        offset = 2;
-    }else{
-        offset = Math.floor((containerElements.length - 2) / 2) + 2;
-    }
-    
-    let num = offset + index;
-    
-    // 要素が存在する場合
-    if (containerElements.length >= (num + 1)) {
-        let fourteenthContainerElement = containerElements[num];
+            fourteenthContainerElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'start'
+            });
 
-        // ブラウザのボトムまでスクロール
-        fourteenthContainerElement.scrollIntoView({
-            behavior: 'smooth', // スムーズなスクロールを有効にする場合
-            block: 'center',       // 要素のボトムに配置
-            inline: 'start'     // 要素の左端に配置
-        });
-
-        // localStorageのfavoriteIndexを更新
-        localStorage.setItem('favoriteIndex', favoriteIndex + 1);
-    } else {
-        alert('Element not found at index:', num);
-    }
+            localStorage.setItem('favoriteIndex', favoriteIndex + 1);
+        } else {
+            alert('Element not found at index:', num);
+        }
+    }, 3000);
 })();
