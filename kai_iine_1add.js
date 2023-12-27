68373c060b813163ee0db10d5c8d217d10e43573
dtakaMoreg <ch2chanchan@gmail.com>
2023-12-27 11:12:14 +0900

    Update kai_iine_1add.js

--- kai_iine_1add.js
+++ kai_iine_1add.js
@@ -5,7 +5,7 @@ javascript:(function() {
     var fileName ="";
     var scroll_count= 0;
     
-    navigator.clipboard.writeText("start")  
+    navigator.clipboard.writeText("start");  
 
     // 指定したIDの要素を取得します
     var userList = document.getElementById('userList');
@@ -95,8 +95,9 @@ javascript:(function() {
         // ローカルストレージに収集したURLを保存
         localStorage.setItem('collectedURLs', JSON.stringify(collectedURLs));
 
+        navigator.clipboard.writeText("end");
         alert('URLを収集しました: current[' + tmplst.length + '] total[' + collectedURLs.length + ']([' + beforecnt + ']->[' + aftercnt + '])');
-        navigator.clipboard.writeText("end")  
+          
     }
 
 })();
