847bc3e4f6e6189f1e835f420f68a22db1035262
dai <dai@dai>
2024-01-02 19:03:16 +0900

    test

--- iinecount.js
+++ iinecount.js
@@ -1,5 +1,16 @@
 javascript:(function(){
     //ローカルストレージをクリップボードに格納
     var collectedURLs = JSON.parse(localStorage.getItem('collectedURLs')) || [];
-    navigator.clipboard.writeText(collectedURLs.length)  
+//    navigator.clipboard.writeText(collectedURLs.length)
+    navigator.clipboard.writeText(collectedURLs.length)
+  .then(() => {
+    // 成功した場合の処理
+    console.log('テキストがクリップボードにコピーされました');
+  })
+  .catch((error) => {
+    // エラーが発生した場合の処理
+    console.error('クリップボードへのコピー中にエラーが発生しました:', error);
+    alert('クリップボードへのコピー中にエラーが発生しました');
+  });
+
 })();
