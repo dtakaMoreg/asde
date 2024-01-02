9de559c76713a3e4110bd4cd1759785125911bf1
dai <dai@dai>
2024-01-02 18:52:48 +0900

    orig

--- iinecount.js
+++ iinecount.js
@@ -2,5 +2,4 @@ javascript:(function(){
     //ローカルストレージをクリップボードに格納
     var collectedURLs = JSON.parse(localStorage.getItem('collectedURLs')) || [];
     navigator.clipboard.writeText(collectedURLs.length)  
-    alert("call script");
 })();
