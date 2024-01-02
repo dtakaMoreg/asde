20eb57266683ed17ecff9046bb39844b299d62a5
dai <dai@dai>
2024-01-02 21:05:25 +0900

    maybe fixed

--- iinecount.js
+++ iinecount.js
@@ -2,19 +2,6 @@ javascript:(function(){
     //ローカルストレージをクリップボードに格納
     var collectedURLs = JSON.parse(localStorage.getItem('collectedURLs')) || [];
 //    navigator.clipboard.writeText(collectedURLs.length)
-    async function copyToClipboard() {
-      try {
-        await navigator.clipboard.writeText(collectedURLs.length);
-        console.log('テキストがクリップボードにコピーされました');
-      } catch (error) {
-        console.error('クリップボードへのコピー中にエラーが発生しました:', error);
-        alert('クリップボードへのコピー中にエラーが発生しました\nエラー内容: ' + error.message);
-      }
-    }
-
-    // ボタンクリックなどのイベント内で呼び出す
-    copyToClipboard();
-
-
+    setTimeout(() => navigator.clipboard.writeText(collectedURLs.length), 500);
 
 })();
