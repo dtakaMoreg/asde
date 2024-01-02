bb371f47db6befcdb94330674480c38ae0ffbd69
dai <dai@dai>
2024-01-02 18:39:12 +0900

    testcode

--- iinecount.js
+++ iinecount.js
@@ -2,4 +2,5 @@ javascript:(function(){
     //ローカルストレージをクリップボードに格納
     var collectedURLs = JSON.parse(localStorage.getItem('collectedURLs')) || [];
     navigator.clipboard.writeText(collectedURLs.length)  
+    alert("call script");
 })();
