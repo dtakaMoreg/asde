7e9202cf13388a6c825f1f4b5b97a2b43778c79a
dai <dai@dai>
2024-09-07 11:27:11 +0900

    e

--- post_room.js
+++ post_room.js
@@ -1,18 +1,13 @@
 javascript:(function() {
-    // 1. クリップボードの内容をtextareaにペースト
+    // クリップボードの内容をtextareaにペースト
     navigator.clipboard.readText().then(function(text) {
         var textarea = document.getElementById('collect-content');
         if (textarea) {
-            textarea.focus();
+            textarea.value = text; // クリップボードの内容をセット
             
-            // 1秒待ってからテキストをセット
-            setTimeout(function() {
-                textarea.value = text; // クリップボードの内容をセット
-                
-                // inputイベントを手動で発火させる
-                var event = new Event('input', { bubbles: true });
-                textarea.dispatchEvent(event);
-            }, 1000);
+            // inputイベントを手動で発火させる
+            var event = new Event('input', { bubbles: true });
+            textarea.dispatchEvent(event);
         }
     }).catch(function(err) {
         console.error('クリップボードの内容を取得できませんでした: ', err);
