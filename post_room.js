5e47decee1528a39047f480101aa209613a01f48
dai <dai@dai>
2024-09-07 11:01:14 +0900

    a

--- post_room.js
+++ post_room.js
@@ -1,19 +1,3 @@
 javascript:(function() {
-    // 1. クリップボードの内容をtextareaにペースト
-    navigator.clipboard.readText().then(function(text) {
-        var textarea = document.getElementById('collect-content');
-        if (textarea) {
-            textarea.value = text; // クリップボードの内容をセット
-        }
-
-        // 2. 2秒後にbuttonをクリック
-        setTimeout(function() {
-            var button = document.querySelector('button.collect-btn');
-            if (button) {
-               // button.click(); // ボタンをクリック
-            }
-        }, 12000); // 2秒待つ
-    }).catch(function(err) {
-        console.error('クリップボードの内容を取得できませんでした: ', err);
-    });
+  console.log(1);
 })();
