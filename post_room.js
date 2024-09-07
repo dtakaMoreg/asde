24a68cd9394db29965cfcc903c28d8e3f268d376
dai <dai@dai>
2024-09-07 11:03:55 +0900

    ab

--- post_room.js
+++ post_room.js
@@ -1,3 +1,11 @@
 javascript:(function() {
-  console.log(1);
+    // 1. クリップボードの内容をtextareaにペースト
+    navigator.clipboard.readText().then(function(text) {
+        var textarea = document.getElementById('collect-content');
+        if (textarea) {
+            textarea.value = text; // クリップボードの内容をセット
+        }
+    }).catch(function(err) {
+        console.error('クリップボードの内容を取得できませんでした: ', err);
+    });
 })();
